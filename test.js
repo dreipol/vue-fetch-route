d1c0d571d0e3bc80bcdad622b111a7010bbe84de
Rouven Bühlmann <rouven.buehlmann@dreipol.ch>
2017-12-08 13:24:59 +0100

    Add basics for tests

--- test.js
+++ test.js
@@ -2,6 +2,11 @@
 
 require('jsdom-global')();
 const assert = require('assert');
+const Vue = require('vue');
+const Vuex = require('vuex');
+const VueRouter = require('vue-router');
+const VueRouteFetch = require('./dist');
+
 
 describe('Tests', function() {
     it('stub', function() {
